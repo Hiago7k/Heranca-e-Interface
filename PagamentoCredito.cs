b8c0de03fe9d956f2112be30e810c86eef489bc4
Hiago 7k <hiago7k@gmail.com>
2026-03-04 21:26:54 -0300

    criando classe pagamento de credito e implementando heranca e interface

---
+++ PagamentoCredito.cs
@@ -0,0 +1,10 @@
+﻿namespace Heranca_e_Interface;
+
+internal class PagamentoCredito : Pessoa, IPagamentos
+{
+    public PagamentoCredito(string nome, string email)
+        :base(nome, email)
+    {
+        
+    }
+}
