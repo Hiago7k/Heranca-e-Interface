226bae8a21e19900c59b8d23647b4650a347b9fa
Hiago 7k <hiago7k@gmail.com>
2026-03-04 21:32:34 -0300

    instanciando nossos objetos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worlds!");
+﻿using Heranca_e_Interface;
+using Heranca_e_Interfacel;
+
+PagamentoCredito cliente1 = new PagamentoCredito("Andre", "andre@gmail.com");
+PagamentoBoleto cliente2 = new PagamentoBoleto("Juliana", "juliana@hotmal.com");
+
+cliente1.ProcessarPagamentos();
+cliente2.ProcessarPagamentos();
