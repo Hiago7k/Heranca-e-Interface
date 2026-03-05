7efcefe36ba9b0a487ee5ae694342e9e6dfac4f5
Hiago 7k <hiago7k@gmail.com>
2026-03-04 21:28:28 -0300

    criando classe pagamento de boleto e implementando heranca e interface

---
+++ PagamentoBoleto.cs
@@ -0,0 +1,12 @@
+﻿using Heranca_e_Interface;
+
+namespace Heranca_e_Interfacel
+{
+    internal class PagamentoBoleto: Pessoa, IPagamentos
+    {
+        public PagamentoBoleto(string nome, string email) :base(nome, email)
+        {
+            
+        }
+    }
+}
