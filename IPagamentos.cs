285ded01a049bf451f600fddb13778e4e48e596a
Hiago 7k <hiago7k@gmail.com>
2026-03-04 21:25:13 -0300

    criando interface de pagamentos

---
+++ IPagamentos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca_e_Interface
+{
+    internal interface IPagamentos
+    {
+    }
+}
