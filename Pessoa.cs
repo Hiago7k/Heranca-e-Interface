8a2ed57f6d41ec0deacff249e3f937564fdc1381
Hiago 7k <hiago7k@gmail.com>
2026-03-04 21:24:24 -0300

    criando a classe Mae

---
+++ Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace Heranca_e_Interface;
+
+internal class Pessoa
+{
+    public Pessoa(string nome, string email)
+    {
+        Nome = nome;
+        Email = email;
+    }
+
+    public string Nome { get;}
+    public string Email { get;}
+}
